34ef96a4e27674c25f730f472d77b17871c879b8
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-07-20 16:04:16 +0200

    Add the health check endpoint

--- techtrends/app.py
+++ techtrends/app.py
@@ -22,7 +22,7 @@ def get_post(post_id):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -30,7 +30,7 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
@@ -45,7 +45,7 @@ def post(post_id):
 def about():
     return render_template('about.html')
 
-# Define the post creation functionality 
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -64,6 +64,10 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
+@app.route('/healthz')
+def health_check():
+    return jsonify({'result': 'OK - healthy'})
 
 # start the application on port 3111
 if __name__ == "__main__":
